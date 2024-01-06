e7c45f50c8c6afca548e8b99cc49241d8c52b19a
SasakinME <max_to_day@mail.ru>
2024-01-06 20:13:25 +0300

    update chart

--- npm/index.js
+++ npm/index.js
@@ -5,45 +5,45 @@ const app = express();
 const Chart = require('chart.js');
 const Decimal = require('decimal.js');
 const parser = require('really-relaxed-json').createParser();
+const path = require('path');
 
 const ejs = require('ejs');
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
 
 async function getData(res) {
     try {
-        const response = await axios.get('https://api.coincap.io/v2/assets/bitcoin/history?interval=d1&start=1630435200000&end=1633027200000');
+        const startDate = new Date('2021-09-01'); // задаем начальную дату
+        const endDate = new Date(); // получаем текущую дату и время
+
+        const startTimestamp = startDate.getTime(); // получаем timestamp начальной даты
+        const endTimestamp = endDate.getTime(); // получаем timestamp текущей даты
+
+        const apiUrl = `https://api.coincap.io/v2/assets/bitcoin/history?interval=d1&start=${startTimestamp}&end=${endTimestamp}`;
+
+        const response = await axios.get(apiUrl); // отправляем запрос с параметризированными датами
+
         const data = response.data.data;
 
         // Extract the required data for the chart
-        const dates = data.map(item => new Date(item.time));
+        const dates= data.map(date => {
+            const currentDate = new Date(date.time);
+            const year = currentDate.getFullYear().toString().slice(2);
+            const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+            const day = currentDate.getDate().toString().padStart(2, '0');
+            return `${year}-${month}-${day}`;
+        });
         const prices = data.map(item => item.priceUsd);
-        app.set('view engine', 'ejs'); // Установка EJS в качестве движка шаблонов
-        res.render = ejs.render
-        res.render('index.ejs', { dates, prices });
+        res.render('index', { dates, prices });
         console.log('График успешно создан');
     } catch (error) {
         console.error(error);
     }
 }
-/*
-function getData(res) {
-    // Ваша логика получения данных
-    const dates = ['01.01.2024', '02.01.2024', '03.01.2024'];
-    const prices = [10, 20, 30];
-
-    res.render('index.ejs', { dates:dates, prices: prices });
-}*/
 
 app.get('/', async (req, res) => {
-    const response = await axios.get('https://api.coincap.io/v2/assets/bitcoin/history?interval=d1&start=1630435200000&end=1633027200000');
-    const data = response.data.data;
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-
-    // Extract the required data for the chart
-    const dates = data.map(item => item.time);
-    const prices = data.map(item => new Decimal(item.priceUsd));
-    res.render('index', {dates, prices});
+    getData(res);
 });
 
 app.use((req, res) => {
